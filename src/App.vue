641b89034ddb82516968ab4389a7f7d4c93e7261
Felipe <jfelipecr94@gmail.com>
2025-07-07 21:38:59 -0400

    create style guide and button component

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,19 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import StyleGuide from './pages/StyleGuide.vue'
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline text-blue-900 p-16">
-    Hello world!
-  </h1>
-  <HelloWorld msg="Vite + Vue" />
+  <div class="app-content w-screen h-screen">
+    <StyleGuide />
+    <HelloWorld msg="Vite + Vue" />
+  </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.app-content {
+  background-image: url(./assets/background-boreal.jpg);
+  background-size: cover;
+  background-position: -33rem;
 }
 </style>
---
+++ src/pages/StyleGuide.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Button from '../components/Button.vue'
+</script>
+
+<template>
+  <div class="style-guide--section p-4 text-neutral-50">
+    <h1 class="text-center text-4xl">Style Guide</h1>
+    <h2 class="text-2xl">Botones</h2>
+    <div class="pl-4 mb-4">
+      <h3 class="text-xl">Primary</h3>
+      <Button label="Primary Button" />
+    </div>
+    <div class="pl-4">
+      <h3 class="text-xl">Primary with icon</h3>
+      <Button label="With Icon" :showIcon="true" />
+    </div>
+  </div>
+</template>
